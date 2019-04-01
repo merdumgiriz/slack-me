0694f84c66a43718d806dda4cc4d2ed47d365066
merdumgiriz <tamertumkaya@gmail.com>
2019-04-01 11:00:34 +0300

    fix

--- main.js
+++ main.js
@@ -19,9 +19,38 @@ app.get('/', function (req, res) {
 
 
 app.post('/action-endpoint', function (req, res) {
-  const challenge = req.body.challenge;
+  const challenge = req.body.challenge; // this challenge is needed to ensure slack that our bot works
+
   const reply = {
       "challenge": challenge
   };
+
+  const headers = {
+    'Content-type': 'application/json',
+    'Authorization': `Bearer ${process.env.TOKEN}` // this token you need to set on heroku
+  }
+
+  // console.log(req.body.event);
+
+  if (req.body.event.subtype != 'bot_message') { // se we won't reply to ourselves...
+    const body = {
+      'channel': req.body.event.channel,
+      'text': req.body.event.text // going to reply with the same text
+    }
+
+    const options = {
+      url:   'https://slack.com/api/chat.postMessage',
+      method: 'POST',
+      headers,
+      body:  JSON.stringify(body)
+    };
+
+    request.post(options, function(err, res, body) {
+      if (err) {
+        console.log(err);
+      }
+    })
+  }
+
   res.json(reply);
 });
