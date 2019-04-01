333c6aec98f5065d2326487ef38d8b1f4a04db7b
merdumgiriz <tamertumkaya@gmail.com>
2019-04-01 10:53:25 +0300

    fix

--- main.js
+++ main.js
@@ -8,9 +8,20 @@ app.use(bodyParser.json());
 const listener = app.listen(process.env.PORT || '3000', function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
 app.get('/', function (req, res) {
     const reply = {
         "status": "ok"
     };
     res.json(reply);
+});
+
+
+app.post('/action-endpoint', function (req, res) {
+  const challenge = req.body.challenge;
+  const reply = {
+      "challenge": challenge
+  };
+  res.json(reply);
 });
